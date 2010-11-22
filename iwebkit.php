f7b855f27e6597ed505c65f294d5166669d2956d
Mattias Wadman <mattias.wadman@gmail.com>
2010-11-22 18:25:34 +0100

    Add moz css extensions for rounding, shadow and gloss Show warning if defined Show per app warning, iOS verison, device model Some HTML and CSS cleanup

--- iwebkit.php
+++ iwebkit.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+  <title>Applications</title>
   <meta content="yes" name="apple-mobile-web-app-capable" />
   <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
   <meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
   <link href="iwebkit/css/style.css" rel="stylesheet" media="screen" type="text/css" />
   <link href="css/appicon.css" rel="stylesheet" media="screen" type="text/css" />
   <script src="iwebkit/javascript/functions.js" type="text/javascript"></script>
-  <title>Applications</title>
   <style type="text/css">
     /* ugly iwebkit overrides */
     .list ul img {
@@ -28,6 +28,18 @@
       margin: 25px 0px 0px 70px !important;
       position: absolute;
     }
+    .list .withimage a, .list .withimage:hover a {
+      height: 70px !important;
+    }
+    .warning {
+      background-color: #f99;
+      height: auto !important;
+    }
+    .warning-requires {
+      /*color: #f99;*/
+      color: #922;
+      font-size: 0.8em;
+    }
   </style>
 </head>
 <body class="list">
@@ -37,24 +49,41 @@
     <div id="title">Applications</div>
   </div>
   <div id="content">
-  <? foreach($manifests as $m) { ?>
     <ul>
+    <?php if(isset($warning)) { ?>
+      <li class="textbox warning"><?=$warning?></li>
+    <?php } ?>
+    <?php foreach($manifests as $m) { ?>
       <li class="withimage">
 	<a class="noeffect" href="<?=$m->link_itms?>">
 	  <span class="icon">
 	    <div class="icon-shadow"></div>
+	    <div class="icon-background"></div>
 	    <div class="<?=$m->ipa->has_prerendered_icon ? "" : "icon-glossy"?>"></div>
+	    <?php if(isset($m->link_icon)) { ?>
 	    <img class="icon-rounded" src="<?=$m->link_icon?>">
+	    <?php } ?>
 	  </span>
 	  <span class="name"><?=htmlspecialchars($m->ipa->display_name)?></span>
 	  <span class="comment">
-	    Version <?=$m->ipa->version?><br>
-	    <?=date("Y-m-d", $m->ipa->created)?><br>
+	    Version <?=$m->ipa->version?>
+	    <?php if(!$m->ipa->ios_version->has_model($ios)) { ?>
+	    <span class="warning-requires">
+	      requires <?=implode_natural(", ", " or ", $m->ipa->ios_version->models)?>
+	    </span>
+	    <?php } else if($ios->less_than($m->ipa->ios_version)) { ?>
+	    <span class="warning-requires">
+	      requires iOS <?=$m->ipa->ios_version->version_string()?>
+	    </span>
+	    <?php } ?>
+	    <br>
+	    <?=date("Y-m-d", $m->ipa->created)?>
+	    <br>
 	  </span>
 	</a>
       </li>
+    <?php } ?>
     </ul>
-  <? } ?>
   </div>
   <div id="footer"></div>
 </body>
--- springboard.php
+++ springboard.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <title>Applications</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta content="yes" name="apple-mobile-web-app-capable" />
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
     <link href="css/appicon.css" rel="stylesheet" media="screen" type="text/css" />
     <style>
@@ -27,7 +29,6 @@
 	margin: 0 auto;
 	text-align: center;
 	width: 32%;
-	height: 100px;
 	float: left;
       }
       .app-display-name {
@@ -42,21 +43,47 @@
 	font-size: 0.1em;
 	color: #eee;
       }
+      .warning {
+	background-color: #f99;
+	height: auto;
+	padding: 5px;
+	border: solid 1px #999;
+	margin: 10px;
+      }
+      .warning-requires {
+	color: #922;
+	font-size: 0.3em;
+      }
     </style>
   </head>
   <body>
-  <div class="gradient">
+    <div class="gradient">
+    <?php if(isset($warning)) { ?>
+      <div class="warning"><?=$warning?></div>
+    <?php } ?>
     <? foreach($manifests as $m) { ?>
       <div class="app">
-	<div class="icon">
-	  <a href="<?=$m->link_itms?>">
-	    <div class="icon-shadow"></div>
-	    <div class="<?=$m->ipa->has_prerendered_icon ? "" : "icon-glossy"?>"></div>
-	    <img class="icon-rounded" src="<?=$m->link_icon?>">
-	  </a>
-	</div>
+	<a href="<?=$m->link_itms?>">
+	  <div class="icon">
+	      <div class="icon-shadow"></div>
+	      <div class="icon-background"></div>
+	      <div class="<?=$m->ipa->has_prerendered_icon ? "" : "icon-glossy"?>"></div>
+	      <?php if(isset($m->link_icon)) { ?>
+	      <img class="icon-rounded" src="<?=$m->link_icon?>">
+	      <?php } ?>
+	  </div>
+	</a>
 	<span class="app-display-name"><a href="<?=$m->link_itms?>"><?=htmlspecialchars($m->ipa->display_name)?></a></span><br>
 	<span class="app-created"><?=date("Y-m-d", $m->ipa->created)?></span><br>
+	<?php if(!$m->ipa->ios_version->has_model($ios)) { ?>
+	<span class="warning-requires">
+	  Requires <?=implode_natural(", ", " or ", $m->ipa->ios_version->models)?>
+	</span>
+	<?php } else if($ios->less_than($m->ipa->ios_version)) { ?>
+	<span class="warning-requires">
+	  Requires iOS <?=$m->ipa->ios_version->version_string()?>
+	</span>
+	<?php } ?>
       </div>
     <? } ?>
     <div style="clear: both"></div>
